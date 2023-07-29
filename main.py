e70d2ec44b3a243c830b427f4dcc0817737752a5
notyasuomain-cmd <notyasuomain22@gmail.com>
2023-07-29 13:12:03 +0200

    .

--- main.py
+++ main.py
@@ -2,37 +2,56 @@ import cv2
 from ultralytics import YOLO
 
 
-
+## load model
 model = YOLO("yolov8n.pt")
+
+
+## webcam
 vid = cv2.VideoCapture(0)
+## class names for prediction
 names = model.names
 
+
+## set resulution
 vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+
 while True:
+    ## get frame
     ret, frame = vid.read()
 
+    ## make prediction with YOLOV8
     results = model.predict(frame)
 
+    ## for each result draw the boxes
     for result in results:
         boxes = result.boxes
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy.tolist()[0]
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+            
             
             predicton = names[int(box.cls.tolist()[0])]
             confidence = box.conf.tolist()[0]
             
-            cv2.putText(frame, "{} {:.2f}".format(predicton, confidence), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            if int(confidence*100) >= 75:
+                ## draw boxes
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+                ## puttext with class name and confidence
+                cv2.putText(frame, "{} {:.2f}".format(predicton, confidence), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+
                       
             
 
-
+    ## show frame
     cv2.imshow('frame', frame)
 
+    ## if q is pressed break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
+## release video and destroy all windows
 vid.release()
 cv2.destroyAllWindows()
