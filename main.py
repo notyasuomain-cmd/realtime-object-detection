edb0e1c74cc44d351a6983287e88f64886132265
notyasuomain-cmd <notyasuomain22@gmail.com>
2023-07-29 11:02:44 +0200

    .

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import cv2
 from ultralytics import YOLO
 
 
+
 model = YOLO("yolov8n.pt")
 vid = cv2.VideoCapture(0)
 names = model.names
 
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while True:
     ret, frame = vid.read()
@@ -17,12 +20,14 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy.tolist()[0]
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
-            predicton = (names[int(box.cls.tolist()[0])])
-        print(boxes.conf)
+            
+            predicton = names[int(box.cls.tolist()[0])]
+            confidence = box.conf.tolist()[0]
+            
+            cv2.putText(frame, "{} {:.2f}".format(predicton, confidence), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                      
+            
 
-        confidence = boxes.conf.tolist()[0]
-
-        cv2.putText(frame, "{} {:.2f}".format(predicton, confidence), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow('frame', frame)
 
