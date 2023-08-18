a3c4695c7561432da8d5472f187d4fd9debe5a12
notyasuomain-cmd <notyasuomain22@gmail.com>
2023-08-18 13:34:09 +0200

    .

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+from dotenv import dotenv_values
 import cv2
 from ultralytics import YOLO
 
 
 # load model
 model = YOLO("yolov8n.pt")
+config = dotenv_values(".env")
 
 
 # webcam
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(f"rtsp://{config['RTSP_USER']}:{config['RTSP_PASSWD']}@{config['CAM_IP']}:{config['RTSP_PORT']}/Streaming/channels/101")
 # class names for prediction
 NAMES = model.names
 print(type(NAMES))
 
 # set resulution
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# vid.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
+# vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 900)
+# vid.set(cv2.CAP_PROP_FPS, 10)
 
+fps = int(vid.get(5))
+print("fps:", fps)
 
 while True:
     # get frame
     ret, frame = vid.read()
+
+    if not ret:
+        print("Camera is disconnected")
+        vid.release()
+        break
 
     # make prediction with YOLOV8
     results = model.predict(frame)  # verbose=False to hide log
@@ -34,20 +44,21 @@ while True:
             predicton = NAMES.get(int(box.cls.tolist()[0]))
             confidence = box.conf.tolist()[0]
 
-            classes = ["person", "bottle", "remote"]
             # if confidence is greater than 75% and prediction in our classes
             #  then draw boxes and put text
-            if int(confidence*100) >= 75 and predicton in classes:
-                # draw boxes
-                cv2.rectangle(frame, (int(x1), int(y1)),
-                              (int(x2), int(y2)), (0, 0, 255), 2)
-                # puttext with class name and confidence
-                cv2.putText(frame, f"{predicton} {confidence:.2f}", (int(
-                    x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            
+            #classes = ["person", "bottle", "remote"]
+            #if int(confidence*100) >= 75 and predicton in classes:
+             
+            # draw boxes
+            cv2.rectangle(frame, (int(x1), int(y1)),
+                            (int(x2), int(y2)), (0, 0, 255), 2)
+            # puttext with class name and confidence
+            cv2.putText(frame, f"{predicton} {confidence:.2f}", (int(
+                x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     # show frame
     cv2.imshow('frame', frame)
-
     # if q is pressed break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
